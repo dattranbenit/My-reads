10fe0c0391152f76e3c92bbc108fb30e39d2833b
nogo0d <jmonteiro205@gmail.com>
2018-02-16 16:35:07 +0000

    FIX: display authors splited by ","

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -32,7 +32,7 @@ class BookShelf extends React.Component {
                                             onBookMove={this.props.onBookMove} />
                                     </div>
                                     <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors}</div>
+                                    <div className="book-authors">{Array.isArray(book.authors)?book.authors.join(', '):''}</div>
                                 </div>
                             </li>
                         ))
